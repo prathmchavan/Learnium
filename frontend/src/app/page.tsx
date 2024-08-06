472b5c1cbf196b6818d2fb2e7c1d95885c26bc8b
Prathmesh Chavan <96901316+prathmchavan@users.noreply.github.com>
2024-08-06 17:01:43 +0530

    initial commit
    Project initialization

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,14 @@
+import { BackgroundBeams } from "@/components/ui/background-beams";
+
+
+
+export default function Home() {
+  return (
+    <div className="relative flex items-center justify-center min-h-screen overflow-hidden">
+    <BackgroundBeams />
+    <h1 className="text-6xl font-bold text-center text-gray-200">
+      Hello, something is coming soon!
+    </h1>
+  </div>
+  );
+}
