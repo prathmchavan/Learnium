a4ae0d6510ef37b8b73ca1a19a967ae38defb5d2
Prathmesh Chavan <96901316+prathmchavan@users.noreply.github.com>
2024-08-08 01:20:28 +0530

    Navbar setup basic

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -14,8 +14,23 @@ module.exports = {
   ],
   darkMode: "class",
   theme: {
-    extend: {},
-  },
+    extend: {
+      animation: {
+        "meteor-effect": "meteor 5s linear infinite",
+      },
+      keyframes: {
+        meteor: {
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
+          "70%": { opacity: "1" },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: "0",
+          },
+        },
+      },
+    },
+  
+},
   plugins: [addVariablesForColors],
 };
 
