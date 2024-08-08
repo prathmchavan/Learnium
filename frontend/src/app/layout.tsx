c0597127fcc5dedc72d0b6629e7c006cb20c7077
Prathmesh Chavan <96901316+prathmchavan@users.noreply.github.com>
2024-08-08 10:05:07 +0530

    minor changes
    Navbar minor changes added dropdown

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/Global/Navbar";
 import { SparklesCore } from "@/components/ui/sparkels";
 
 const inter = Inter({ subsets: ["latin"] });
