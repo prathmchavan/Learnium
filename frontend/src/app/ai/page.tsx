ebe91a7b1d3f2c4cf505fa24dec45ead9a6927c2
Prathmesh Chavan <96901316+prathmchavan@users.noreply.github.com>
2024-08-09 20:12:37 +0530

    firebase genkit initialized

---
+++ frontend/src/app/ai/page.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { callMenuSuggestionFlow } from '@/app/genkit';
+import { useState } from 'react';
+
+export default function Ai() {
+  const [menuItem, setMenu] = useState<string>('');
+
+  async function getMenuItem(formData: FormData) {
+    const theme = formData.get('theme')?.toString() ?? '';
+    const suggestion = await callMenuSuggestionFlow(theme);
+    setMenu(suggestion);
+  }
+
+  return (
+    <main>
+      <form action={getMenuItem}>
+        <label>
+          Suggest a menu item for a restaurant with this theme:{' '}
+        </label>
+        <input type="text" name="theme" className='text-black' />
+        <button type="submit">Generate</button>
+      </form>
+      <br />
+      <pre>{menuItem}</pre>
+    </main>
+  );
+}
