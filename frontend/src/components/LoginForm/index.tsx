d537052351e05bcd644b14a5ce313b6bc7916d7b
Prathmesh Chavan <96901316+prathmchavan@users.noreply.github.com>
2024-08-22 19:45:21 +0530

    Authenticationi ui done
    Ui for login and signup done

--- frontend/src/components/LoginForm/index.tsx
+++ frontend/src/components/LoginForm/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import Form from './Form'
+import { Form } from './Form'
+
 
 const LoginForm = () => {
   return (
-    <div className=' bg-gradient-to-r from-[#0d0dcf51] via-[#000000] to-[#0d0dcf51]'>
+    <div className=' bg-gradient-to-r from-[#0d0dcf51] via-[#000000] to-[#0d0dcf51] p-16'>
         <Form/>
     </div>
   )
