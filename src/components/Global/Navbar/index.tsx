f9650a70035cdb3c5eeac209168ca24d7351cc9d
Prathmesh Chavan <96901316+prathmchavan@users.noreply.github.com>
2024-08-25 22:29:04 +0530

    authentication page update
    1) login and signup page ui done
    2) auth functionality done
    3) proper userflow remaining for better user experience

--- src/components/Global/Navbar/index.tsx
+++ src/components/Global/Navbar/index.tsx
@@ -1,18 +1,21 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { TailwindcssButtons } from "../TailwindcssButtons";
 import Link from "next/link";
 import Image from "next/image";
-
+import { useAuthContext } from "@/context/AuthContext";
 
 const Navbar = () => {
+    const { user ,logout } = useAuthContext();
+
+
     return (
         <div className="bg-white bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-5 flex px-11 py-3">
-            <div className="flex justify-start  items-center">
+            <div className="flex justify-start items-center">
                 <Image src={'/logo/log.svg'} width={150} height={150} alt="logo" />
             </div>
             {/* Centered navigation items */}
-            <ul className="flex flex-row justify-center items-center  gap-11 flex-grow text-[#E5E6FF]">
+            <ul className="flex flex-row justify-center items-center gap-11 flex-grow text-[#E5E6FF]">
                 <li>
                     <Link href={'/'}>Home</Link>
                 </li>
@@ -33,12 +36,18 @@ const Navbar = () => {
                 </li>
             </ul>
 
-            {/* Button at the end */}
-            <div className="flex justify-end  items-center">
-                <TailwindcssButtons name="Login / Signup" link="login" />
-            </div>
-        </div>
+            {!user && (
+                <div className="flex justify-end items-center">
+                    <TailwindcssButtons name="Login / Signup" link="login" />
+                </div>
+            )}
 
+            {user && (
+                <div className="flex justify-end items-center">
+                    <TailwindcssButtons name="Logout" onClick={logout} />
+                </div>
+            )}
+        </div>
     );
 };
 
---
+++ src/constant/secrets.ts
@@ -0,0 +1,3 @@
+export const ApiUrl ="https://free-ap-south-1.cosmocloud.io/development/api/";
+export const ProjectId ='66c833e54559bc7988068cc0';
+export const EnviromentId ='66c833e54559bc7988068cc1';
