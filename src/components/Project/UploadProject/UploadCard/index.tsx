8a7f812e40818d4b40d26e1998f83e85e6e3467e
Prathmesh Chavan <chavanprathmesh40@gmail.com>
2024-09-10 17:54:39 +0530

    Project page update 1.1
    upload card added to project list page

---
+++ src/components/Project/UploadProject/UploadCard/index.tsx
@@ -0,0 +1,15 @@
+import { GlareCard } from "@/components/ui/glare-card"
+
+const UploadCard = () => {
+    return (
+        <div className="mx-5 my-5">
+            <GlareCard className="flex flex-col items-center justify-center">
+                <p className="text-white  text-sm text-center px-5 mt-4">🚀 Ready to showcase your masterpiece? Upload your project now and let the world see your brilliance! 💡✨
+                    Whether it's groundbreaking tech or a creative twist, this is your moment to shine! 🌟
+                    Hit that Upload button and get your project the recognition it deserves! 📈🔥</p>
+         <h1 className="text-white font-bold text-2xl mt-4">Share Your Project !</h1>
+            </GlareCard>
+        </div>
+    );
+}
+export default UploadCard;
