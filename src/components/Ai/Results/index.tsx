ef387995f9a9243f6afb1b7d1102b9a4a7bcda4b
Prathmesh Chavan <chavanprathmesh40@gmail.com>
2024-10-07 16:10:28 +0530

    Update index.tsx

--- src/components/Ai/Results/index.tsx
+++ src/components/Ai/Results/index.tsx
@@ -35,7 +35,7 @@ const Results: React.FC = () => {
 
     return (
 
-        <div className="flex flex-col items-center justify-center min-h-screen  p-4  backdrop-blur-2xl">
+        <div className="flex flex-col items-center justify-center min-h-screen  p-4 backdrop-blur-2xl">
             {isAptiResult && (
                 <div>
                     <h1 className="text-3xl font-bold mb-4">Test Results</h1>
